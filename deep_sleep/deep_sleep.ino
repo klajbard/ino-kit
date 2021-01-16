f8281c8c8df923adf50d07b962f228617a277c14
klajbar <klajbard@gmail.com>
2021-01-16 08:27:35 +0100

    Extend deep sleep example with button wake up

--- deep_sleep/deep_sleep.ino
+++ deep_sleep/deep_sleep.ino
@@ -1,6 +1,10 @@
 void setup() {
   Serial.begin(115200);
   while(!Serial) { }
+
+  // To allow wake up on button press uncomment the following lines
+  // Serial.println("I'm awake, but I'm going into deep sleep mode until RESET pin is connected to a LOW signal");
+  // ESP.deepSleep(0);
 }
 
 void loop() {
